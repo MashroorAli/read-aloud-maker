2f63d6829da405b8a7d965844defda78d3e87460
Mashroor Ali <alimashroor1@gmail.com>
2026-09-08 17:55:14 -0400

    added privacy policy

--- app.py
+++ app.py
@@ -310,5 +310,9 @@ def delete_folder(folder_id):
 
     return redirect("/")
 
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
